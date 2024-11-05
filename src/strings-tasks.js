a2f85eff5d5b99c899b794d5bea4558a737b6946
Daria Leskina <dleskinaleskina@yandex.ru>
2024-11-05 18:27:06 +0300

    feat: 21-26 solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -206,8 +206,8 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -221,8 +221,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -238,8 +238,12 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const minutesStr = String(minutes).padStart(2, '0');
+
+  const secondsStr = String(seconds).padStart(2, '0');
+
+  return `${minutesStr}:${secondsStr}`;
 }
 
 /**
